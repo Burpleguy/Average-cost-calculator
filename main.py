6f904296129f51ca1dce0f0ee42d8b05b7298ea8
kingkendrew06 <shen.kendrew@gmail.com>
2022-08-03 03:10:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy
+import random
+
+#user input phase
+a = input("Enter first value:")
+print(a)
+b = input("Enter second value:")
+print(b)
+c = input("Enter third value:")
+print(c)
+d = input("Enter fourth value:")
+print(d)
+
+
+
+avg = []
+avg.append(a)
+avg.append(b)
+avg.append(c)
+avg.append(d)
+
+print(avg)
+
+#print(x)
+
